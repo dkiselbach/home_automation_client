1ff401095c3d0e8e917501a1fc8f157713dc331d
dkiselbach <dylankiselbach@gmail.com>
2022-07-01 09:08:48 -0500

    Update header

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,13 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import {
-    useQuery,
-    gql, useSubscription
-} from "@apollo/client";
+import './App.css';
+import Header from './components/Header'
 
 function App() {
-    const [count, setCount] = useState(0)
-
-    const { loading, error, data } = useQuery(gql`
-          {      
-            homes {
-                id
-                name
-            }
-          }
-    `)
-
-    if (loading) {
-        return <p>loading...</p>;
-    }
-
-    if (error) {
-        return <pre>{JSON.stringify(error, null, 2)}</pre>;
-    }
-
-    return (
-        <div className="App">
-            {data.homes &&
-                data.homes.map((home) => (
-                    <div key={home.id}>
-                        <h3>{home.name}</h3>
-                    </div>
-                ))}
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      {props.children}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import {
-    ApolloClient, ApolloProvider,
-    InMemoryCache
-} from "@apollo/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
-    cache: new InMemoryCache()
+  dataIdFromObject: (o) => o.id,
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
 });
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <ApolloProvider client={client}>
-          <App />
-      </ApolloProvider>
-  </React.StrictMode>
-)
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path={'/'} component={App}></Route>
+      </Router>
+    </ApolloProvider>
+  </React.StrictMode>,
+);
